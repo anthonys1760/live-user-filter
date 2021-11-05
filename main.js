d37f210f77e7e49504cd5a234e6bcda2f99d7531
Anthony Smith <anthony.smith1760@gmail.com>
2021-11-05 01:57:42 -0400

    File Beautify

--- main.js
+++ main.js
@@ -10,7 +10,9 @@ filter.addEventListener('input', (e) => filterData(e.target.value))
 async function getData() {
     const res = await fetch('https://randomuser.me/api?results=50');
 
-    const{ results } = await res.json();
+    const {
+        results
+    } = await res.json();
 
     //Clear results
     result.innerHTML = '';
